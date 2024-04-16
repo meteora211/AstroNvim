1945a8b3e6cae867f633de3b4abc633877644100
meteora <czhaor@gmail.com>
2024-04-16 09:00:09 +0000

    update fugitive mapping.

--- lua/plugins/mapping.lua
+++ lua/plugins/mapping.lua
@@ -71,6 +71,9 @@ return {
 
           -- quick save
           -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+          -- fugitive mapping
+          ["<Leader>gb"] = {"<cmd>Git blame<cr>", desc = "fugitive blame"},
         },
         t = {
           -- setting a mapping to false will disable it
